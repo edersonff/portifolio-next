548b440fb89712083fb632b93e058f8709dadf0e
ederson <ederson@bemobile.tech>
2024-06-23 13:20:16 -0300

    feat: projects animation with swiper

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,5 +1,9 @@
 import { Project } from "@/@types/project";
-import React from "react";
+import Image from "next/image";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { CgMaximizeAlt } from "react-icons/cg";
 
 const projects: Project[] = [
   {
@@ -100,5 +104,228 @@ const projects: Project[] = [
 ];
 
 export default function Projects() {
-  return <div>Projects</div>;
+  const [selectedProject, setSelectedProject] = useState<number>(-1);
+  const [transition, setTransition] = useState<boolean>(false);
+  const [maximized, setMaximized] = useState<boolean>(false);
+
+  function startTransition() {
+    if (transition) return;
+
+    setTransition(true);
+
+    const timeout = setTimeout(() => {
+      setTransition(false);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }
+
+  const video = useMemo(() => {
+    const project = projects[selectedProject];
+
+    if (selectedProject === -1) {
+      return undefined;
+    }
+
+    if (!project.video) {
+      return "/videos/transition.mp4";
+    }
+
+    return `/projects/${projects[selectedProject].video}`;
+  }, [selectedProject]);
+
+  console.log(video);
+
+  const showMaximized = useMemo(() => {
+    const project = projects[selectedProject];
+
+    if (selectedProject === -1) {
+      return false;
+    }
+
+    return project.video !== undefined;
+  }, [selectedProject]);
+
+  useEffect(() => {
+    document.body.style.overflow = maximized ? "hidden" : "auto";
+  }, [maximized]);
+
+  return (
+    <>
+      <div className="relative">
+        <div className="absolute w-full top-0 flex gap-[30px] h-48 select-none pointer-events-none">
+          <div className="flex-1 relative z-20">
+            <Image
+              src="/images/icons/double-underline.svg"
+              alt="Icone de linha dupla"
+              width={85}
+              height={20}
+              onDragStart={(e) => e.preventDefault()}
+              className="unselectable undraggable absolute -bottom-12 left-1/2 transform -translate-x-1/2"
+            />
+          </div>
+          <div className="flex-1 relative z-20">
+            <Image
+              src="/images/icons/asterisk.svg"
+              alt="Icone de asterisco"
+              width={30}
+              height={40}
+              onDragStart={(e) => e.preventDefault()}
+              className="unselectable undraggable absolute left-4 bottom-3"
+            />
+            <Image
+              src="/images/icons/chevrons.svg"
+              alt="Icone de duas setas"
+              width={55}
+              height={55}
+              onDragStart={(e) => e.preventDefault()}
+              className="unselectable undraggable absolute z-20 -right-1 -top-3"
+            />
+          </div>
+          <div className="flex-1 relative z-20">
+            <Image
+              src="/images/icons/bar-chart.svg"
+              alt="Icone de gráfico de barras"
+              width={35}
+              height={38}
+              onDragStart={(e) => e.preventDefault()}
+              className="unselectable undraggable absolute left-4 bottom-3"
+            />
+            <Image
+              src="/images/icons/pointer.svg"
+              alt="Icone de ponteiro"
+              width={39}
+              height={52}
+              onDragStart={(e) => e.preventDefault()}
+              className="unselectable undraggable absolute -right-4 -bottom-8"
+            />
+          </div>
+          <div className="flex-1 relative z-20">
+            <Image
+              src="/images/icons/check.svg"
+              alt="Icone de check"
+              width={25}
+              height={36}
+              onDragStart={(e) => e.preventDefault()}
+              className="unselectable undraggable absolute right-2 top-2"
+            />
+          </div>
+        </div>
+
+        <div className="h-48 flex">
+          <Swiper
+            modules={[Autoplay]}
+            spaceBetween={30}
+            slidesPerView={4}
+            loop
+            autoplay={{
+              delay: 5000,
+            }}
+            pagination={{ clickable: true }}
+            className="h-full"
+          >
+            {projects.map((item, i) => (
+              <SwiperSlide key={item.name}>
+                <div
+                  className="flex-1 h-full border-[5px] border-black bg-white hard-shadow relative cursor-pointer"
+                  onClick={() => {
+                    setSelectedProject(i);
+                    startTransition();
+                  }}
+                >
+                  <div className="absolute-full bg-black bg-opacity-50 z-10 opacity-0 transition-opacity duration-300">
+                    <div className="flex-center w-full h-full">
+                      <p className="text-white font-bold text-2xl">Ver mais</p>
+                    </div>
+                  </div>
+                  <Image
+                    src={`/projects/${item.image}`}
+                    alt={`Imagem do projeto ${item.name}`}
+                    width={300}
+                    height={300}
+                    className="absolute-full"
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+
+        <div className="absolute -bottom-[43vh] -right-[5vw] z-20 group">
+          {showMaximized && (
+            <div
+              className="absolute rounded-full bg-black bg-opacity-50 p-2 flex items-center gap-2 cursor-pointer transition-opacity duration-300 opacity-0 group-hover:opacity-100"
+              onClick={() => setMaximized(!maximized)}
+            >
+              <CgMaximizeAlt size={20} />
+              <p className="text-xs font-bold">Ver em tela cheia</p>
+            </div>
+          )}
+
+          <Image
+            src="/images/illustrations/laptop.svg"
+            alt="Ilustração de um laptop isométrico"
+            width={400}
+            height={400}
+            onDragStart={(e) => e.preventDefault()}
+            className="unselectable undraggable"
+          />
+          <div
+            className="absolute top-0 bg-black"
+            style={{
+              width: 341,
+              height: 194,
+              left: 111,
+              top: 61,
+              transform: "rotateX(29deg) rotateY(50deg) rotateZ(0deg)",
+            }}
+          >
+            {transition ? (
+              // /videos/transitions.mp4
+              <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="absolute-full object-cover"
+                src="/videos/transition.mp4"
+              />
+            ) : (
+              <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="absolute-full object-cover"
+                src={video}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+
+      {maximized && (
+        <div
+          className="fixed-full z-[999] bg-black bg-opacity-90 flex-center"
+          aria-modal
+        >
+          <div
+            className="relative w-full h-full"
+            onClick={() => setMaximized(false)}
+          >
+            <div className="absolute-full z-10"></div>
+            <div className="absolute-full z-20 flex-center p-10 pointer-events-none">
+              <video
+                autoPlay
+                loop
+                controls
+                className="max-w-full max-h-full pointer-events-auto"
+                src={video}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
