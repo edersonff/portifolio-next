db630a7b4f387e3168a896510f3c076f9c3bfff9
ederson <ederson@bemobile.tech>
2024-06-23 13:20:27 -0300

    fix: swiper provider added in providers

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import FontProvider from "./font";
 import "../app/globals.css";
 import AlertProvider from "./alert";
+import SwiperProvider from "./swiper";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <FontProvider>
-      <AlertProvider>{children}</AlertProvider>
+      <SwiperProvider>
+        <AlertProvider>{children}</AlertProvider>
+      </SwiperProvider>
     </FontProvider>
   );
 }
