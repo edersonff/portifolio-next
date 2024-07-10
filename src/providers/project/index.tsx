1f34177822e3bd60595f7e448ff2a0a08a4273d3
edersonff <edersonff_@hotmail.com.br>
2024-07-10 15:02:29 -0300

    fix: project overlay not working interaction

--- src/providers/project/index.tsx
+++ src/providers/project/index.tsx
@@ -9,27 +9,20 @@ export default function ProjectProvider() {
 
   if (maximized)
     return (
-      <>
+      <div className="fixed-full z-[99999999] flex-center" aria-modal>
         <div
-          className="fixed-full z-[999] bg-black bg-opacity-90 flex-center"
-          aria-modal
-        >
-          <div
-            className="relative w-full h-full"
-            onClick={() => setMaximized(undefined)}
-          >
-            <div className="absolute-full z-10"></div>
-            <div className="absolute-full z-20 flex-center p-10 pointer-events-none">
-              <video
-                autoPlay
-                loop
-                controls
-                className="max-w-full max-h-full pointer-events-auto"
-                src={maximized}
-              />
-            </div>
-          </div>
+          className="absolute-full bg-black/75"
+          onClick={() => setMaximized(undefined)}
+        ></div>
+        <div className="absolute-full flex-center p-10 pointer-events-none">
+          <video
+            autoPlay
+            loop
+            controls
+            className="max-w-full max-h-full pointer-events-auto"
+            src={maximized}
+          />
         </div>
-      </>
+      </div>
     );
 }
