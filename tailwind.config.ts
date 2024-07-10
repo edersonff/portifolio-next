78b883519b280958f8fbbe5a36f4de8572c46dcd
edersonff <edersonff_@hotmail.com.br>
2024-07-10 14:58:44 -0300

    fix: tailwind compiler folders

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,7 @@ const mainSizes = {
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/providers/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
