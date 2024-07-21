546852d3527093b065ca39a9608c73efe1b46ac6
edersonff <edersonff_@hotmail.com.br>
2024-07-21 20:41:31 -0300

    feat: lang types

--- src/hooks/useLang/index.tsx
+++ src/hooks/useLang/index.tsx
@@ -1,7 +1,9 @@
 import { useSearchParams } from "next/navigation";
 
-export default function useLang() {
+type Lang = "pt-br" | "en";
+
+export default function useLang(): Lang {
   const searchParams = useSearchParams();
 
-  return searchParams.get("l") || "pt-br";
+  return (searchParams.get("l") as Lang) || "pt-br";
 }
