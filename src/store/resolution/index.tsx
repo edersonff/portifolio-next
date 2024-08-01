c526ceebde60d08579a254d7a7b10063bad7f049
edersonff <edersonff_@hotmail.com.br>
2024-07-31 23:06:06 -0300

    fix: default resolution set to "max"

--- src/store/resolution/index.tsx
+++ src/store/resolution/index.tsx
@@ -10,7 +10,7 @@ export type ResolutionStore = {
 
 const resolutionStore = persist<ResolutionStore>(
   (set) => ({
-    resolution: "min",
+    resolution: "max",
     setResolution: (resolution) => {
       set({ resolution });
     },
